aea05e4d8590ba71d37348c18dedd3c58a5783e4
Dangog <cajedanilo@gmail.com>
2024-03-31 22:03:40 -0300

    Added Token Methods

--- src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -1,7 +1,10 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.infra.security.TokenService;
 import med.voll.api.model.DadosAutenticacao;
+import med.voll.api.model.JWTTokenData;
+import med.voll.api.model.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,11 +21,16 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
-        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-        var authenticaon = manager.authenticate(token);
+        var authenticationTokentoken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication = manager.authenticate(authenticationTokentoken);
 
-        return ResponseEntity.ok().build();
+        var token = tokenService.generateToken((Usuario) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new JWTTokenData(token));
     }
 }
---
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -0,0 +1,41 @@
+package med.voll.api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import jakarta.validation.Valid;
+import med.voll.api.model.Usuario;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(Usuario user){
+        System.out.println(secret);
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("Vollmed API")
+                    .withSubject(user.getLogin())
+                    .withClaim("id", user.getId())
+                    .withExpiresAt(expirationData())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            // Invalid Signing configuration / Couldn't convert Claims.
+            throw new RuntimeException("Generate JWT Error", exception);
+        }
+    }
+
+    private Instant expirationData() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
