0534116a60510e9b6d117188c02ef9a3eb0a1265
Dangog <cajedanilo@gmail.com>
2024-04-03 01:43:12 -0300

    Added Rest configurations

--- src/main/java/med/voll/api/infra/security/TokenFilter.java
+++ src/main/java/med/voll/api/infra/security/TokenFilter.java
@@ -4,7 +4,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -14,27 +17,32 @@ import java.io.IOException;
 public class TokenFilter extends OncePerRequestFilter {
 
     @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
     private TokenService tokenService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = getToken(request);
 
-        var subject = tokenService.getSubject(token);
+        if (token != null){
+            var subject = tokenService.getSubject(token);
+            var user = usuarioRepository.findByLogin(subject);
 
-        System.out.println(subject);
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);}
 
         filterChain.doFilter(request,response);
     }
 
     private String getToken(HttpServletRequest request) {
         var authorizationHeader = request.getHeader("Authorization");
-;
 
-        if (authorizationHeader == null){
-            throw new RuntimeException("Invalid or Null Access Token");
+        if (authorizationHeader != null){
+            return authorizationHeader.replace("Bearer ","");
         }
-
-        return authorizationHeader.replace("Bearer ","");
+        return null;
     }
 }
